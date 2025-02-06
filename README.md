ed053765d37b25bc556f37c0c1c3dd4336736d7a
vyooo <vynxinqx@gmail.com>
2025-02-06 20:24:57 +0700

    kamusgaul.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# myproject_kodland
-testing
---
+++ kamusgaul.py
@@ -0,0 +1,13 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "SHEESH": "sedikit ketidaksetujuan",
+            "GTG": "got to go"
+            }
+
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print('maaf kami tidak paham')
