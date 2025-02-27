add672483e18aa4c6d55af9a6602f796a099a27b
vyooo <vynxinqx@gmail.com>
2025-02-27 20:27:17 +0700

    Create mybot.py

---
+++ mybot.py
@@ -0,0 +1,25 @@
+import discord
+
+# Variabel intents menyimpan hak istimewa bot
+intents = discord.Intents.default()
+# Mengaktifkan hak istimewa message-reading
+intents.message_content = True
+# Membuat bot di variabel klien dan mentransfernya hak istimewa
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Kita telah masuk sebagai {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$halo'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
+    
+client.run('add your token")
